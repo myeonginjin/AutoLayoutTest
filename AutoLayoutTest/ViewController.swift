cb147de2030fac029b36de15d470c941500d1c84
myeonginjin <113578420+myeonginjin@users.noreply.github.com>
2024-02-23 01:56:35 +0900

    [develop] 필터 편집 화면 반응형 UI 구현

--- AutoLayoutTest/ViewController.swift
+++ AutoLayoutTest/ViewController.swift
@@ -1,122 +1,125 @@
-//
-//  ViewController.swift
-//  AutoLayoutTest
-//
-//  Created by Jaein Yoo on 2/20/24.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    private let toptabBar : UIView = UIView()
+    private let galleryBtn : UIButton = UIButton()
+    private let toggleBtn : UIButton = UIButton()
+    private let saveBtn : UIButton = UIButton()
+    private let imageView : UIImageView = UIImageView()
+    private let editBar : UIView = UIView()
+    private let slider1 : UISlider = UISlider()
+    private let slider2 : UISlider = UISlider()
+    private let likeBtn : UIButton = UIButton()
+    private let filterView : UIView  = UIView()
     private let inputImage = CIImage(image: UIImage(named: "input.jpg")!)
-    
-    lazy var imageView: UIImageView = {
-        
-        let imageView = UIImageView(frame: self.view.frame)
-        
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-
-        imageView.image = UIImage(ciImage: inputImage!)
-
-    
-        return imageView
-        }()
-    
-    lazy var slider: UISlider = {
-//        let greenSlider = UISlider(frame: .init())
-//        greenSlider.layer.position = CGPoint(x:self.view.frame.midX, y:500)
-        
-        let greenSlider = UISlider(frame: CGRect(x:0, y:0, width:200, height:30))
-        
-        
-          
-        greenSlider.layer.position = CGPoint(x:self.view.frame.midX, y:700)
-
-        
-//        greenSlider.translatesAutoresizingMaskIntoConstraints = false
-        greenSlider.backgroundColor = UIColor.white
-        greenSlider.layer.cornerRadius = 10.0
-        greenSlider.layer.shadowOpacity = 0.5
-        greenSlider.layer.masksToBounds = false
-        greenSlider.minimumValue = 0
-        greenSlider.maximumValue = 1
-        greenSlider.value = 0.5
-        greenSlider.maximumTrackTintColor = UIColor.gray
-        greenSlider.minimumTrackTintColor = UIColor.black
-        greenSlider.addTarget(self, action: #selector(onChangeValueSlider(sender:)), for: .valueChanged)
-        return greenSlider
-              }()
-
 
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        
+        toptabBar.translatesAutoresizingMaskIntoConstraints = false
+        galleryBtn.translatesAutoresizingMaskIntoConstraints = false
+        toggleBtn.translatesAutoresizingMaskIntoConstraints = false
+        saveBtn.translatesAutoresizingMaskIntoConstraints = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        editBar.translatesAutoresizingMaskIntoConstraints = false
+        slider1.translatesAutoresizingMaskIntoConstraints = false
+        slider2.translatesAutoresizingMaskIntoConstraints = false
+        likeBtn.translatesAutoresizingMaskIntoConstraints = false
+        filterView.translatesAutoresizingMaskIntoConstraints = false
+        
+        toptabBar.backgroundColor = .gray
+        imageView.backgroundColor = .gray
+        editBar.backgroundColor = .gray
+        filterView.backgroundColor = .gray
+        galleryBtn.backgroundColor = .white
+        toggleBtn.backgroundColor = .white
+        saveBtn.backgroundColor = .white
+        slider1.backgroundColor = .white
+        slider2.backgroundColor = .white
+        likeBtn.backgroundColor = .white
         
         
-        self.view.addSubview(self.imageView)
+        toptabBar.addSubview(galleryBtn)
+        toptabBar.addSubview(toggleBtn)
+        toptabBar.addSubview(saveBtn)
         
-        self.view.addSubview(self.slider)
+        
+        imageView.image = UIImage(ciImage: inputImage!)
+        
+        editBar.addSubview(slider1)
+        editBar.addSubview(slider2)
+        editBar.addSubview(likeBtn)
+        
+        
+        self.view.addSubview(toptabBar)
+        self.view.addSubview(imageView)
+        self.view.addSubview(editBar)
+        self.view.addSubview(filterView)
 
-        self.view.backgroundColor = UIColor.white
-        self.view.backgroundColor = UIColor(red: 0, green: CGFloat(slider.value), blue: 0, alpha: 1)
 
         
         NSLayoutConstraint.activate([
-             imageView.topAnchor.constraint(equalTo: self.view.topAnchor),
-             imageView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
-             imageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-             imageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-//             slider.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 100),
-//             slider.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 100),
-//             slider.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -100)
+            toptabBar.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 24),
+            toptabBar.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            toptabBar.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            toptabBar.heightAnchor.constraint(equalToConstant: 50),
+
+            galleryBtn.leadingAnchor.constraint(equalTo: toptabBar.leadingAnchor, constant: 8),
+            galleryBtn.centerYAnchor.constraint(equalTo: toptabBar.centerYAnchor),
+            galleryBtn.heightAnchor.constraint(equalToConstant: 50),
+            galleryBtn.widthAnchor.constraint(equalToConstant: 30),
+
+            saveBtn.trailingAnchor.constraint(equalTo: toptabBar.trailingAnchor, constant: -8),
+            saveBtn.centerYAnchor.constraint(equalTo: toptabBar.centerYAnchor),
+            saveBtn.heightAnchor.constraint(equalToConstant: 50),
+            saveBtn.widthAnchor.constraint(equalToConstant: 30),
+
+            toggleBtn.trailingAnchor.constraint(equalTo: saveBtn.leadingAnchor, constant: -8),
+            toggleBtn.centerYAnchor.constraint(equalTo: toptabBar.centerYAnchor),
+            toggleBtn.heightAnchor.constraint(equalToConstant: 50),
+            toggleBtn.widthAnchor.constraint(equalToConstant: 30),
+            
+            imageView.topAnchor.constraint(equalTo: toptabBar.bottomAnchor, constant: 24),
+            imageView.bottomAnchor.constraint(equalTo:editBar.topAnchor, constant: -16),
+            imageView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            imageView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: 8),
+            
+            
+            editBar.heightAnchor.constraint(equalToConstant: 50),
+            editBar.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            editBar.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            editBar.bottomAnchor.constraint(equalTo:filterView.topAnchor, constant: -16),
+            
+            slider1.leadingAnchor.constraint(equalTo: editBar.leadingAnchor, constant: 24),
+            slider1.topAnchor.constraint(equalTo: editBar.topAnchor, constant: 8),
+            slider1.bottomAnchor.constraint(equalTo: editBar.bottomAnchor, constant: -8),
+            slider1.widthAnchor.constraint(equalToConstant: 126),
+            
+            
+            slider2.leadingAnchor.constraint(equalTo: slider1.trailingAnchor, constant: 24),
+            slider2.topAnchor.constraint(equalTo: editBar.topAnchor, constant: 8),
+            slider2.bottomAnchor.constraint(equalTo: editBar.bottomAnchor, constant: -8),
+            slider2.widthAnchor.constraint(equalToConstant: 126),
+            
+            likeBtn.trailingAnchor.constraint(equalTo: editBar.trailingAnchor, constant: -24),
+            likeBtn.topAnchor.constraint(equalTo: editBar.topAnchor, constant: 8),
+            likeBtn.bottomAnchor.constraint(equalTo: editBar.bottomAnchor, constant: -8),
+            likeBtn.widthAnchor.constraint(equalToConstant: 30),
+            
+            filterView.heightAnchor.constraint(equalToConstant: 100),
+            filterView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            filterView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            filterView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -56),
         ])
         
-
-        
         
     }
     
     
-    
-    
-    override func didReceiveMemoryWarning() {
-        
-        
-          
-        super.didReceiveMemoryWarning()
-    
-        }
-    
-
-    @objc func onChangeValueSlider(sender: UISlider){
-
-        self.view.backgroundColor = UIColor(red: 0, green: CGFloat(sender.value), blue: 0, alpha: 1)
-
-        let colorFilter = CIFilter(name: "CIColorCrossPolynomial")
-
-        colorFilter!.setValue(self.inputImage, forKey: kCIInputImageKey)
 
 
-        let r: [CGFloat] = [0.0, CGFloat(sender.value), 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-
-        let g: [CGFloat] = [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-
-        let b: [CGFloat] = [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-
-        colorFilter!.setValue(CIVector(values: r, count: 10), forKey: "inputRedCoefficients")
-
-        colorFilter!.setValue(CIVector(values: g, count: 10), forKey: "inputGreenCoefficients")
-
-        colorFilter!.setValue(CIVector(values: b, count: 10), forKey: "inputBlueCoefficients")
-
-        let outputImage : CIImage = colorFilter!.outputImage!
-
-
-        self.imageView.image = UIImage(ciImage: outputImage)
-        self.imageView.setNeedsDisplay()
-
-    }
 
 
 }
